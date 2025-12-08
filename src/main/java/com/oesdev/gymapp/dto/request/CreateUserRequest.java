00ab9710344ed2c87b1d3be19fad002c956be60f
Omar Salum <o.e.salum@gmail.com>
2025-12-08 00:46:02 -0300

    Terminar de agregar validaciones

---
+++ src/main/java/com/oesdev/gymapp/dto/request/CreateUserRequest.java
@@ -0,0 +1,44 @@
+package com.oesdev.gymapp.dto.request;
+
+import com.oesdev.gymapp.entity.Adress;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+public class CreateUserRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String lastname;
+
+    @Pattern(regexp = "^[0-9]{7,8}$", message = "El DNI debe tener solo números (7 a 8 dígitos)")
+    private String dni; //Unique
+
+    @Size(min = 5, message = "El nombre de usuario debe tener 5 o más carácteres")
+    private String username; //Unique
+
+    private String password;
+
+    @NotBlank
+    @Email
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
+    private String email; //Unique
+
+    @Pattern(regexp = "^\\+?[0-9\\s\\-()]{7,20}$\n")
+    private String phoneNumber;
+
+    private String emergencyPhoneNumber;
+
+    private Adress adress;
+
+}
