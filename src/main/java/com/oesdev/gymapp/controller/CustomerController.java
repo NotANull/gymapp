565fa7a52b25bf87c8595e2fc9e5f34fef89e629
Omar <o.e.salum@gmail.com>
2025-12-21 13:38:53 -0300

    Hacer service

--- src/main/java/com/oesdev/gymapp/controller/CustomerController.java
+++ src/main/java/com/oesdev/gymapp/controller/CustomerController.java
@@ -1,17 +1,33 @@
 package com.oesdev.gymapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.oesdev.gymapp.dto.request.CreateCustomerRequest;
+import com.oesdev.gymapp.dto.response.CustomerDetailsResponse;
+import com.oesdev.gymapp.entity.Routine;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
+    private final ICustomerService iCustomerService;
+
+    public CustomerController(ICustomerService iCustomerService) {
+        this.iCustomerService = iCustomerService;
+    }
+
+    //Only test
     @GetMapping("/{id}")
     public String getCustomer(@PathVariable Long id) {
         return "hola cliente con id " + id;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CustomerDetailsResponse> createCustomer(@RequestBody @Valid CreateCustomerRequest request) {
+        CustomerDetailsResponse response = this.iCustomerService.createCustomer(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 }
