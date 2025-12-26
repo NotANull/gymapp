66913b59808cab7c8b8ac632a2488583ae73e50c
Omar <o.e.salum@gmail.com>
2025-12-26 10:31:57 -0300

    Lista rutinas AFUERA, lo crea entity

--- src/main/java/com/oesdev/gymapp/dto/request/CreateCustomerRequest.java
+++ src/main/java/com/oesdev/gymapp/dto/request/CreateCustomerRequest.java
@@ -2,11 +2,7 @@ package com.oesdev.gymapp.dto.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.*;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +12,6 @@ public class CreateCustomerRequest {
 
     @NotNull
     private CreateMembershipRequest membership;
-
-    private List<CreateRoutineRequest> assignedRoutines;
 
     @Valid
     @NotNull
