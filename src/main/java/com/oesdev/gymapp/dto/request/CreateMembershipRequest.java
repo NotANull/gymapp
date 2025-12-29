eee62fad948959fec1ff5dc29d8fbab0d8e57819
Omar <o.e.salum@gmail.com>
2025-12-29 10:02:11 -0300

    imports sin usar

--- src/main/java/com/oesdev/gymapp/dto/request/CreateMembershipRequest.java
+++ src/main/java/com/oesdev/gymapp/dto/request/CreateMembershipRequest.java
@@ -1,14 +1,11 @@
 package com.oesdev.gymapp.dto.request;
 
 import com.oesdev.gymapp.enums.PlanType;
-import com.oesdev.gymapp.enums.Status;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/oesdev/gymapp/service/CustomerServiceImp.java
+++ src/main/java/com/oesdev/gymapp/service/CustomerServiceImp.java
@@ -1,17 +1,12 @@
 package com.oesdev.gymapp.service;
 
 import com.oesdev.gymapp.dto.request.CreateCustomerRequest;
-import com.oesdev.gymapp.dto.response.AddressDetailsResponse;
 import com.oesdev.gymapp.dto.response.CustomerDetailsResponse;
-import com.oesdev.gymapp.dto.response.MembershipDetailsResponse;
-import com.oesdev.gymapp.dto.response.UserDetailsResponse;
 import com.oesdev.gymapp.entity.CustomerProfile;
-import com.oesdev.gymapp.enums.Status;
 import com.oesdev.gymapp.mapper.CustomerMapper;
 import com.oesdev.gymapp.repository.ICustomerRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
