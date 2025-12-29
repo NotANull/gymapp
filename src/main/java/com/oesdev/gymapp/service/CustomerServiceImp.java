74c7275dc18ec489418f668618e29baf8cfe54aa
Omar <o.e.salum@gmail.com>
2025-12-28 23:52:03 -0300

    Terminar de corregir

--- src/main/java/com/oesdev/gymapp/service/CustomerServiceImp.java
+++ src/main/java/com/oesdev/gymapp/service/CustomerServiceImp.java
@@ -1,23 +1,38 @@
 package com.oesdev.gymapp.service;
 
 import com.oesdev.gymapp.dto.request.CreateCustomerRequest;
+import com.oesdev.gymapp.dto.response.AddressDetailsResponse;
 import com.oesdev.gymapp.dto.response.CustomerDetailsResponse;
+import com.oesdev.gymapp.dto.response.MembershipDetailsResponse;
+import com.oesdev.gymapp.dto.response.UserDetailsResponse;
+import com.oesdev.gymapp.entity.CustomerProfile;
+import com.oesdev.gymapp.enums.Status;
+import com.oesdev.gymapp.mapper.CustomerMapper;
+import com.oesdev.gymapp.repository.ICustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class CustomerServiceImp implements ICustomerService{
 
-    private ICustomerRepository iCustomerRepository;
+    private final ICustomerRepository iCustomerRepository;
+    private final CustomerMapper customerMapper;
 
-    public CustomerServiceImp(ICustomerRepository iCustomerRepository) {
+    public CustomerServiceImp(ICustomerRepository iCustomerRepository, CustomerMapper customerMapper) {
         this.iCustomerRepository = iCustomerRepository;
+        this.customerMapper = customerMapper;
     }
 
     @Override
     public CustomerDetailsResponse createCustomer(CreateCustomerRequest request) {
+
+        CustomerProfile entity = customerMapper.toCustomerProfile(request);
+        this.iCustomerRepository.save(entity);
+
         return null;
+
     }
 
     @Override
