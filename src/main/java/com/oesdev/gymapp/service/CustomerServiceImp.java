3535dd52a81706d7e0f5593bca101beb614d6cf4
Omar <o.e.salum@gmail.com>
2025-12-30 11:12:45 -0300

    Validar estados en mÃÃetodo get

--- src/main/java/com/oesdev/gymapp/service/CustomerServiceImp.java
+++ src/main/java/com/oesdev/gymapp/service/CustomerServiceImp.java
@@ -3,11 +3,13 @@ package com.oesdev.gymapp.service;
 import com.oesdev.gymapp.dto.request.CreateCustomerRequest;
 import com.oesdev.gymapp.dto.response.CustomerDetailsResponse;
 import com.oesdev.gymapp.entity.CustomerProfile;
+import com.oesdev.gymapp.exception.CustomerNotFoundException;
 import com.oesdev.gymapp.mapper.CustomerMapper;
 import com.oesdev.gymapp.repository.ICustomerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImp implements ICustomerService{
@@ -23,16 +25,19 @@ public class CustomerServiceImp implements ICustomerService{
     @Override
     public CustomerDetailsResponse createCustomer(CreateCustomerRequest request) {
 
-        CustomerProfile entity = customerMapper.toCustomerProfile(request);
+        CustomerProfile entity = this.customerMapper.toCustomerProfile(request);
         this.iCustomerRepository.save(entity);
 
-        return null;
+        return this.customerMapper.toCustomerResponse(entity);
 
     }
 
     @Override
     public CustomerDetailsResponse getCustomer(Long id) {
-        return null;
+        //Para más adelante, validar los estados
+        CustomerProfile entity = this.iCustomerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
+
+        return this.customerMapper.toCustomerResponse(entity);
     }
 
     @Override
