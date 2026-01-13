fa4e6119370204a2ed5e36620a296bd3b0752888
Omar Salum <o.e.salum@gmail.com>
2026-01-13 11:03:02 -0300

    Continuar con el service

---
+++ src/main/java/com/oesdev/gymapp/controller/MembershipController.java
@@ -0,0 +1,52 @@
+package com.oesdev.gymapp.controller;
+
+import com.oesdev.gymapp.dto.request.CreateMembershipRequest;
+import com.oesdev.gymapp.dto.response.MembershipDetailsResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/memberships")
+public class MembershipController {
+
+    private final IMembershipService iMembershipService;
+
+    public MembershipController(IMembershipService iMembershipService) {
+        this.iMembershipService = iMembershipService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MembershipDetailsResponse> createMembership(@RequestBody CreateMembershipRequest request) {
+
+        MembershipDetailsResponse response = this.iMembershipService.createMembership(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<MembershipDetailsResponse> getMembership(@PathVariable Long id) {
+        MembershipDetailsResponse response = this.iMembershipService.getMembership(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MembershipDetailsResponse>> getMemberships() {
+        return ResponseEntity.ok(this.iMembershipService.getAllMemberships());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<MembershipDetailsResponse> updateMembership(@PathVariable Long id, @RequestBody updateMembershipRequest request) {
+        MembershipDetailsResponse response = this.iMembershipService.updateMembership(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteMembership(@PathVariable Long id) {
+        this.iMembershipService.deleteMembership(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
