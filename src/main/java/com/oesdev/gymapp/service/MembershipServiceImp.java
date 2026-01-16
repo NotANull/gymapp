d07b9bdb2c8801c5e1eacc0ad5116c381843e784
Omar Salum <o.e.salum@gmail.com>
2026-01-15 23:32:54 -0300

    Se agregan los metodos para modificar y eliminar

--- src/main/java/com/oesdev/gymapp/service/MembershipServiceImp.java
+++ src/main/java/com/oesdev/gymapp/service/MembershipServiceImp.java
@@ -1,6 +1,7 @@
 package com.oesdev.gymapp.service;
 
 import com.oesdev.gymapp.dto.request.CreateMembershipRequest;
+import com.oesdev.gymapp.dto.request.updateMembershipRequest;
 import com.oesdev.gymapp.dto.response.MembershipDetailsResponse;
 import com.oesdev.gymapp.entity.Membership;
 import com.oesdev.gymapp.exception.MembershipNotFoundException;
@@ -8,7 +9,6 @@ import com.oesdev.gymapp.mapper.MembershipMapper;
 import com.oesdev.gymapp.repository.IMembershipRepository;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -49,12 +49,20 @@ public class MembershipServiceImp implements IMembershipService {
     }
 
     @Override
-    public MembershipDetailsResponse updateMembership(Long id, BigDecimal newPrice) {
-        return null;
+    public MembershipDetailsResponse updateMembership(Long id, updateMembershipRequest request) {
+
+        Membership membershipEntity = this.iMembershipRepository.findById(id).orElseThrow(() -> new MembershipNotFoundException(id));
+        this.membershipMapper.updateMembershipFromRequest(membershipEntity, request);
+        this.iMembershipRepository.save(membershipEntity);
+
+        return this.membershipMapper.toMembershipResponse(membershipEntity);
     }
 
     @Override
     public void deleteMembership(Long id) {
+
+        Membership membershipEntity = this.iMembershipRepository.findById(id).orElseThrow(() -> new MembershipNotFoundException(id));
+        this.iMembershipRepository.delete(membershipEntity);
 
     }
 }
