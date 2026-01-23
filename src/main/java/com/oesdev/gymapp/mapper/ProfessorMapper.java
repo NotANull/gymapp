f904cccbef11690930e4be0b732bcc147188fcde
NotANull <o.e.salum@gmail.com>
2026-01-23 11:17:30 -0300

    Continuar con los updates

---
+++ src/main/java/com/oesdev/gymapp/mapper/ProfessorMapper.java
@@ -0,0 +1,7 @@
+package com.oesdev.gymapp.mapper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfessorMapper {
+}
--- src/main/java/com/oesdev/gymapp/service/ProfessorServiceImp.java
+++ src/main/java/com/oesdev/gymapp/service/ProfessorServiceImp.java
@@ -1,7 +1,47 @@
 package com.oesdev.gymapp.service;
 
+import com.oesdev.gymapp.dto.request.CreateProfessorRequest;
+import com.oesdev.gymapp.dto.request.UpdateProfessorRequest;
+import com.oesdev.gymapp.dto.response.ProfessorDetailsResponse;
+import com.oesdev.gymapp.mapper.ProfessorMapper;
+import com.oesdev.gymapp.repository.IProfessorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProfessorServiceImp {
+public class ProfessorServiceImp implements IProfessorService{
+
+    private final IProfessorRepository iProfessorRepository;
+    private final ProfessorMapper professorMapper;
+
+    public ProfessorServiceImp(IProfessorRepository iProfessorRepository, ProfessorMapper professorMapper) {
+        this.iProfessorRepository = iProfessorRepository;
+        this.professorMapper = professorMapper;
+    }
+
+    @Override
+    public ProfessorDetailsResponse createProfessor(CreateProfessorRequest request) {
+        return null;
+    }
+
+    @Override
+    public ProfessorDetailsResponse getProfessor(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<ProfessorDetailsResponse> getProfessors() {
+        return List.of();
+    }
+
+    @Override
+    public ProfessorDetailsResponse updateProfessor(Long id, UpdateProfessorRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteProfessor(Long id) {
+
+    }
 }
